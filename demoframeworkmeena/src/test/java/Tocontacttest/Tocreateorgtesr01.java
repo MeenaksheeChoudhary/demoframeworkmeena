2a3c15f5ef71f495d03502ab4d59cb5c111ad948
amans <amans@LAPTOP-EVO2OP8M>
2024-11-05 09:15:29 +0530

    push

---
+++ demoframeworkmeena/src/test/java/Tocontacttest/Tocreateorgtesr01.java
@@ -0,0 +1,10 @@
+package Tocontacttest;
+
+public class Tocreateorgtesr01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("choudhary");
+	}
+
+}
